db745ea16b0d002cd737ddd4672d6e2b82206373
isaax <tabazco_saac@protonmail.com>
2022-01-27 19:00:52 -0500

    var fix

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "dockerhub_credentials" { 
   type = string
-  //default     = "arn:aws:secretsmanager:us-east-1:460475058893:secret:docker-token-07oSpz" 
+  default     = "${var.dockerhub_credentials}"
 }
 variable "codestar_connector_credentials" {
   type = string
-  //default     = "arn:aws:codestar-connections:us-east-1:460475058893:connection/6214a430-eb42-4b1c-8c94-cebe2f341d2f"
+  default     = "${var.codestar_connector_credentials}"
 }
 
 variable region {
@@ -12,3 +12,4 @@ variable region {
   default = "us-east-1"
   
 }
+
